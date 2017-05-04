894663d4dc6490d981a438fdd6b7ed7a8d19b3a4
Bruker <Bruker@10.100.54.230>
2017-05-04 13:17:47 +0200

    Animals

--- OVERVIEW.md
+++ OVERVIEW.md
@@ -1,10 +1,24 @@
 # Oversikt
 
 Student:
+Tom Rune Sæverås
+Yap006
+tomrunesaeveras@hotmail.no
 
 ## Om funksjonaliteten
 
 ## Svar på spørsmål
+1. The Position.move() takes in a direction in which the object should move, and a distance of which the object should move. If the distance is equals to zero, which means that there isn't any longer to move the object, it returns the position it's in before it was asked to move "0 distance". If not it moves the object in the direction that's asked, and returns the new position, by making a new position using the Position.makePos() method. 
+It's different from the Position class in lab5 because in lba5 it uses private double values, that's changed continuously, instead of making new positions. 
+
+2. The difference between AbstractSimObject and AbstractMovingObject is that SimObject is the class that is moving the object. MovingObject figures out the spees of which the object should be moving in. 
+
+3. The other sub-classes can change the position by calling public- methods to change the position.
+
+4. directionTo calculates a direction towards another position. distanceTo calculates the distance to another position. 
+
+5. I would say that it would be smart to make the speed-variable private, because otherwise other methods and class can change it's value, even though it's originally set as protected. It can still be changed even though it's "protected". 
+
 
 ## Kilder til media
 
--- src/inf101/simulator/objects/examples/SimAnimal.java
+++ src/inf101/simulator/objects/examples/SimAnimal.java
@@ -1,4 +1,7 @@
 package inf101.simulator.objects.examples;
+
+import java.util.function.Consumer;
+
 import inf101.simulator.Direction;
 import inf101.simulator.GraphicsHelper;
 import inf101.simulator.Habitat;
@@ -7,24 +10,41 @@ import inf101.simulator.Position;
 import inf101.simulator.SimMain;
 import inf101.simulator.objects.AbstractMovingObject;
 import inf101.simulator.objects.IEdibleObject;
+import inf101.simulator.objects.ISimListener;
 import inf101.simulator.objects.ISimObject;
 import inf101.simulator.objects.SimEvent;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
-public class SimAnimal extends AbstractMovingObject {
+public class SimAnimal extends AbstractMovingObject implements ISimListener {
 	private static final double defaultSpeed = 1.0;
-	private Habitat habitat;
+	private static Habitat habitat;
+	private Image img = MediaHelper.getImage("pipp.png");
+	private double energyLevel = 1;
+	private static final double MAX_ENERGY = 1.0;
+	private static final double MIN_ENERGY = 0.0;
+	private static final double VIEW_DISTANCE = 400;
+	private static final double VIEW_ANGLE = 40;
+	public static final Consumer<GraphicsContext> PAINTER = (GraphicsContext context) -> {
+		SimAnimal obj = new SimAnimal(new Position(0, 0), habitat);
+		context.scale(0.6 / obj.getWidth(), 0.8 / obj.getHeight());
+		obj.draw(context);
+	};
 
 	public SimAnimal(Position pos, Habitat hab) {
 		super(new Direction(0), pos, defaultSpeed);
 		this.habitat = hab;
+		hab.addListener(this, this);
 	}
 
 	@Override
 	public void draw(GraphicsContext context) {
 		super.draw(context);
-		context.fillOval(0, 0, getWidth(), getHeight());
+		context.drawImage(img, 1.0, 0.0, getWidth(), getHeight());
+		super.drawBar(context, energyLevel, 0, Color.PINK, Color.BLUE);
+		GraphicsHelper.strokeArcAt(context, getWidth() / 2, getHeight() / 2, VIEW_DISTANCE, 0, VIEW_ANGLE);
+		context.setStroke(Color.YELLOW.deriveColor(0.0, 1.0, 1.0, 0.5));
 	}
 
 	public IEdibleObject getBestFood() {
@@ -32,11 +52,24 @@ public class SimAnimal extends AbstractMovingObject {
 	}
 
 	public IEdibleObject getClosestFood() {
-		for (ISimObject obj : habitat.nearbyObjects(this, getRadius()+400)) {
-			if(obj instanceof IEdibleObject)
-				return (IEdibleObject) obj;
+		double shorttDist = 401;
+		ISimObject closestObject = null;
+		for (ISimObject obj : habitat.nearbyObjects(this, getRadius() + 400)) {
+
+			if (obj instanceof IEdibleObject) {
+				double tempDist = this.distanceTo(Position.makePos((obj).getX(), obj.getY()));
+				if (tempDist < shorttDist) {
+					closestObject = obj;
+					shorttDist = tempDist;
+				}
+				// food.add((IEdibleObject) obj);
+				// Compare compare = new Compare();
+				// Collections.sort(food, compare);
+				// return getBestFood();
+			}
+			return (IEdibleObject) closestObject;
 		}
-		
+
 		return null;
 	}
 
@@ -49,24 +82,62 @@ public class SimAnimal extends AbstractMovingObject {
 	public double getWidth() {
 		return 50;
 	}
-	
+
 	@Override
 	public void step() {
-		// by default, move slightly towards center
-		dir = dir.turnTowards(directionTo(habitat.getCenter()), 0.5);
+
+		if (energyLevel > MIN_ENERGY) {
+			energyLevel -= 0.0009;
+		}
+
+		IEdibleObject obj = getClosestFood();
+		if (obj != null) {
+			dir = dir.turnTowards(super.directionTo(obj), 2);
+			if (this.distanceToTouch(obj) < 5) {
+				double howMuchToEat = obj.getNutritionalValue();
+				obj.eat(howMuchToEat);
+				if (energyLevel < MAX_ENERGY) {
+					energyLevel += howMuchToEat / 12;
+				}
+				SimEvent event = new SimEvent(this, "CUUUUNT", null, null);
+				habitat.triggerEvent(event);
+				energyLevel += howMuchToEat;
+			}
+		}
+		for (ISimObject rep : habitat.allObjects()) {
+			if (rep instanceof SimRepellant) {
+				if (distanceTo(rep) < 200) {
+					Direction dir1 = directionTo(rep);
+					Direction dir2 = dir1.turnBack();
+					dir = dir.turnTowards(dir2, 2.2);
+				}
+			}
+		}
 
 		// go towards center if we're close to the border
 		if (!habitat.contains(getPosition(), getRadius() * 1.2)) {
 			dir = dir.turnTowards(directionTo(habitat.getCenter()), 5);
-			if (!habitat.contains(getPosition(), getRadius())) { 
+			if (!habitat.contains(getPosition(), getRadius())) {
 				// we're actually outside
 				accelerateTo(5 * defaultSpeed, 0.3);
 			}
 		}
 
-
 		accelerateTo(defaultSpeed, 0.1);
+
+		if (energyLevel < MIN_ENERGY) {
+			super.destroy();
+			SimEvent dead = new SimEvent(this, "DEAD CUNT", null, null);
+			habitat.triggerEvent(dead);
+		}
 
 		super.step();
 	}
+
+	@Override
+	public void eventHappened(SimEvent event) {
+		super.say(event.getType());
+
+	}
+
 }
